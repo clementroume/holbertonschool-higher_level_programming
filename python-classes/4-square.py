905127b10beaaad2d296b0f0d4b93676d0423c33
clementroume <clement.roume@hotmail.com>
2024-11-20 14:24:14 +0100

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -23,16 +23,3 @@ class Square:
     def area(self):
         """area method"""
         return self._Square__size**2
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
