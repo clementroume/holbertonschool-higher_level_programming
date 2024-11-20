c91de2096543d19702615f2248489cfe6949b954
clementroume <clement.roume@hotmail.com>
2024-11-20 12:14:24 +0100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module containt a single class named Square
+"""
+
+
+class Square:
+    """
+    This is the empty class named Square.
+    The empy block is identified using the pass statement.
+    The private instance atribute size is defined.
+    """
+
+    __size = 0
+    pass
