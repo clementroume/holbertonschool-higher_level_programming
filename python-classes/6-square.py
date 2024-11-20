9c92fa9deab96f02392eafacf8ba565b1200c5be
clementroume <clement.roume@hotmail.com>
2024-11-20 15:29:44 +0100

    Coordinates of a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -57,19 +57,3 @@ class Square:
             for j in range(self._Square__size):
                 print(" " * a, end="")
                 print("#" * self._Square__size)
-
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
