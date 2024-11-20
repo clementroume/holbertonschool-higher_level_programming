5cd355fe68e4f95abb961352cb2f958816d9c360
clementroume <clement.roume@hotmail.com>
2024-11-20 13:51:15 +0100

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,7 +10,8 @@ class Square:
     This is the empty class named Square.
     The empy block is identified using the pass statement.
     The private instance atribute size is defined.
-    Added size validaition throught the to if conditions
+    Size validation throught the two if conditions
+    Area calculation throught area(self) function
     """
 
     __size = 0
@@ -24,20 +25,3 @@ class Square:
 
     def area(self):
         return self._Square__size**2
-
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
