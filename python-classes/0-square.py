077c3fb77b8db8478ada855f05015e5ab805bb4a
clementroume <clement.roume@hotmail.com>
2024-11-20 12:08:43 +0100

    My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module containt a single empty class named Square
+"""
+
+
+class Square:
+    """
+    This is the empty class named Square.
+    The empy block is identified using the pass statement
+    """
+
+    pass
