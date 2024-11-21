67d6a59fcdbf7f580d4fb628b61865f4c4c2328a
clementroume <clement.roume@hotmail.com>
2024-11-21 12:07:04 +0100

    Text identation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""4. Text indentation"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    t = text.replace(". ", "\n\n").replace("? ", "\n\n").replace(": ", "\n\n")
+    print(t)
